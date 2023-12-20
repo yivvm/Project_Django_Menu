c7ba974ed87e92daff2fd37ae3bf36e7de8285e7
yivvm <134717898+yivvm@users.noreply.github.com>
2023-12-19 21:43:24 -0500

    Add files via upload

---
+++ littlelemon-menu/workplace/littlelemon/restaurant/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, re_path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('book/', views.book, name="book"),
+
+    # Add the remaining URL path configurations here
+    path('menu/', views.menu, name="menu"),
+    path('menu_item/<int:pk>/', views.display_menu_item, name="menu_item"),
+]
